9f85d7c9edeb59161ba22afc5af14bdee5dacd07
BlackIron <violoniste2@gmail.com>
2023-08-13 20:43:20 +0700

    Initial

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("task1:");
+
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + middleName + lastName;
+
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    public static void task2() {
+        System.out.println("task2:");
+
+
+    }
+
+    public static void task3() {
+        System.out.println("task3:");
+
+
+    }
+}
